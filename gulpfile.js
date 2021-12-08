7f4f21adbe5b9c7f914d57192ab11fd69b6177db
owenroberts <obroberts@gmail.com>
2021-12-08 16:39:45 -0500

    updated script and names

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ const devFiles = [
 function jsTask() {
 	return src(jsFiles)
 		.pipe(sourcemaps.init())
-		.pipe(concat('garden.min.js'))
+		.pipe(concat('game.min.js'))
 		.pipe(uglify())
 		.pipe(sourcemaps.write('./src_maps'))
 		.pipe(dest('./build'))
